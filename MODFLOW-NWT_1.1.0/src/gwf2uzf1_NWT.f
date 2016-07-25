c8c949e550e2374f56208b89acb988bc5575b6b9
Richard Niswonger <rniswon@usgs.gov>
2016-07-25 15:56:53 -0700

    fixed options error found by Richard Winston in UZF1

--- MODFLOW-NWT_1.1.0/src/gwf2uzf1_NWT.f
+++ MODFLOW-NWT_1.1.0/src/gwf2uzf1_NWT.f
@@ -250,33 +250,19 @@ C1------IDENTIFY PACKAGE AND INITIALIZE.
             WRITE(iout,*)
             found = .true.
           case('REJECTSURFK')
-            if ( Ireadsurfk == 1 ) then
               Isurfkreject = 1
               WRITE(iout,*)
               WRITE(IOUT,'(A)')'INFILTRATION WILL BE REJECTED USING '
      +                    ,'LAND SURFACE K'
               WRITE(iout,*)
-            else
-              WRITE(iout,*)
-              WRITE(IOUT,'(A)')'WARNING REJECTSURFK SPECIFIED BUT ',
-     +                     'NO SURFK SPECIFIED. OPTION IGNORED'
-              WRITE(iout,*) 
-            end if
             found = .true.
           case('SEEPSURFK')
-            if ( Ireadsurfk == 1 ) then
               Iseepreject = 1
               WRITE(iout,*)
               WRITE(IOUT,'(A)')'SURFACE LEAKAGE WILL BE CALCULATED ',
      +                         'USING LAND SURFACE K'
               WRITE(iout,*)
-            else
-              WRITE(iout,*)
-              WRITE(IOUT,'(A)')'WARNING SEEPSURFK SPECIFIED BUT ',
-     +                     'NO SURFK SPECIFIED. OPTION IGNORED'
-              WRITE(iout,*) 
-            end if
-            found = .true.
+              found = .true.
         case ('NETFLUX')
             CALL URWORD(LINE,LLOC,ISTART,ISTOP,2,UNITRECH,R,IOUT,IN)
             IF(UNITRECH.LT.0) UNITRECH=0
@@ -298,6 +284,31 @@ C1------IDENTIFY PACKAGE AND INITIALIZE.
         exit
         end select
       end do
+      end if
+ !
+      if ( Ireadsurfk == 0 .and. Isurfkreject == 1) then
+          WRITE(iout,*)
+          WRITE(IOUT,'(A)')'WARNING REJECTSURFK SPECIFIED BUT ',
+     +                    'NO SURFK SPECIFIED. OPTION IGNORED'
+          WRITE(iout,*) 
+          Isurfkreject = 0
+      end if
+!            
+      if ( Ireadsurfk == 0 .and. Iseepreject == 1 ) then
+          WRITE(iout,*)
+          WRITE(IOUT,'(A)')'WARNING SEEPSURFK SPECIFIED BUT ',
+     +                     'NO SURFK SPECIFIED. OPTION IGNORED'
+          WRITE(iout,*) 
+          Iseepreject = 0
+      end if
+!
+      if ( Iseepsupress > 0 .and. Iseepreject == 1 ) then
+          WRITE(iout,*)
+          WRITE(IOUT,'(A)')'WARNING NOSURFLEAK SPECIFIED BUT ',
+     +                     'SEEPSURFK WAS SPECIFIED. SEEPSURFK ',
+     +                     'OPTION IGNORED'
+          WRITE(iout,*) 
+          Iseepreject = 0
       end if
  !
       IF ( INETFLUX.GT.0 ) THEN
