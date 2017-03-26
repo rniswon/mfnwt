1fdc7127c1aabb04a3e80d0dd69ea4d90ba3022b
GS\rniswon <rniswon@usgs.gov>
2017-03-25 18:07:03 -0700

    added shared vars to urguement list for MODSIM

--- MODFLOW-NWT_1.1.2/src/gwf2sfr7_NWT.f
+++ MODFLOW-NWT_1.1.2/src/gwf2sfr7_NWT.f
@@ -8443,7 +8443,7 @@ C
       END FUNCTION smooth
 C
 C-------SUBROUTINE SFR2MODSIM
-      SUBROUTINE SFR2MODSIM()
+      SUBROUTINE SFR2MODSIM(FLOWIN, FLOWOT, SEEPNET)
 C     *******************************************************************
 C     COMPUTE FLOW AND DELTA FLOW OVER THE SEGMENT FOR MIDSOM.
 !--------MARCH 8, 2017
@@ -8454,15 +8454,20 @@ C     -------------------------------------------------------------------
 C     SPECIFICATIONS:
 C     -------------------------------------------------------------------
 C     ARGUMENTS
+      DOUBLE PRECISION, INTENT(INOUT) :: FLOWIN, FLOWOT, SEEPNET
 C     -------------------------------------------------------------------
 !      INTEGER 
 !      DOUBLE PRECISION 
 C     -------------------------------------------------------------------
 C     LOCAL VARIABLES
 C     -------------------------------------------------------------------
-      INTEGER ISTSG, IRNUM, L
-      DOUBLE PRECISION FLOWIN, FLOWOT
+      INTEGER :: ISTSG, IRNUM, L, ISTSGOLD
 C     -------------------------------------------------------------------
+C
+        SEEPNET = 0.0
+        FLOWIN = 0.0
+        FLOWOT = 0.0
+        ISTSG = 0
 C
 C1------LOOP OVER REACHES TO SET FLOWS
 C
@@ -8470,20 +8475,25 @@ C2------DETERMINE LAYER, ROW, COLUMN OF EACH REACH.
         DO l = 1, NSTRM
 C
 C4------DETERMINE STREAM SEGMENT AND REACH NUMBER.
+          ISTSGOLD = ISTSG
           ISTSG = ISTRM(4, l)
-          IRNUM = ISTRM(5, l)         
+          IRNUM = ISTRM(5, l)  
 C
-C3------SET FLOWIN EQUAL TO STREAM SEGMENT INFLOW IF FIRST REACH.
+C5------SUM GAINS/LOSSES FOR ALL REACHES IN SEGMENT.
+          IF( ISTSG /= ISTSGOLD ) SEEPNET = 0.0
+          SEEPNET = SEEPNET + STRM(11, l)
+C
+C6------SET FLOWIN EQUAL TO STREAM SEGMENT INFLOW IF FIRST REACH.
           IF ( IRNUM.EQ.1 ) THEN
             FLOWIN = STRM(10, l)
           END IF
-C4------CHECK IF LAST REACH IN SEGMENT TO GET OUTFLOW    
+C7------CHECK IF LAST REACH IN SEGMENT TO GET OUTFLOW    
           IF ( IRNUM.EQ.ISEG(4,ISTSG)) THEN 
             FLOWOT = STRM(9,l)
           END IF
         END DO
 C
-C5------RETURN.
+C8------RETURN.
       RETURN
       END SUBROUTINE SFR2MODSIM
 C
