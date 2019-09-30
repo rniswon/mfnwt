aab26722711637f62b68c98be3149714dabe553b
Joshua Larsen <jlarsen@usgs.gov>
2019-09-30 11:13:32 -0700

    travis linux debugging

--- autotest/t000_test.py
+++ autotest/t000_test.py
@@ -1,6 +1,8 @@
 # Remove the temp directory and then create a fresh one
 import os
 import shutil
+import subprocess as sp
+import platform
 
 
 def test_setup():
@@ -8,6 +10,13 @@ def test_setup():
     if os.path.isdir(tempdir):
         shutil.rmtree(tempdir)
     os.mkdir(tempdir)
+
+    if platform.system().lower() == 'windows':
+        pass
+    else:
+        sp.Popen(["chmod", "a+rwx", "temp"],stdout=sp.PIPE,
+                    stderr=sp.STDOUT, cwd=".",
+                    shell=True)
     return
 
 
